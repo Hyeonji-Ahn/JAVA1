1803866918a5e3b987772edc2652807e1d8580f0
hyeonjiahn <hyeonjiahn@172.20.1.153>
2020-09-16 22:33:14 -0400

    first push

---
+++ JAVA1/src/Q_and_A.java
@@ -0,0 +1,32 @@
+ // Question and Answers mini-project by Tiffany Ahn
+ // did not need ant help on this one
+
+public class Q_and_A {
+
+	public static void main(String[] args) {
+		System.out.println("Where are you from?");
+		System.out.println("I am from South Korea");
+		System.out.println("");
+		
+		System.out.println("What is your hobby?");
+		System.out.println("I like to watch movies, dance into k-pop songs, and tap dance.");
+		System.out.println("");
+		
+		System.out.println("What computer language did you studied?");
+		System.out.println("I studied html,css,php,java,python,c,and mysql");
+		System.out.println("");
+		
+		System.out.println("Who is your favorite singer?");
+		System.out.println("I like japanese singer Yonezu Kenshi");
+		System.out.println("");
+		
+		System.out.println("What is your favorite food?");
+		System.out.println("I like sushi, cake, chocolate, and mostly any kind of meat");
+		System.out.println("");
+		
+		System.out.println("Do you have siblings?");
+		System.out.println("I have one younger brother who is 12 years old");
+		System.out.println("");
+		
+	}
+}
