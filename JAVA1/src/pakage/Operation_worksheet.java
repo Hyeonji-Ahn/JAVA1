20f93c8283adc000a515a04ea299c6117d6a4b44
hyeonjiahn <hyeonjiahn@172.20.1.153>
2020-09-24 15:25:44 -0400

    Week2

---
+++ JAVA1/src/pakage/Operation_worksheet.java
@@ -0,0 +1,27 @@
+package pakage;
+
+public class Operation_worksheet {
+	public void Integer_to_character(int x) {
+		System.out.println((char)x);
+	}
+	
+	public void Print_last_digit(int x) {
+		int lastdigit = x%10;
+		System.out.println(lastdigit);
+	}
+	
+	public void Exact_Average(int x, int y) {
+		double dx =  x;
+		double dy = y;
+		double avg = (dx+dy)/2;
+		System.out.println(avg);
+	}
+	
+	public void main(String[] args) {
+		Operation_worksheet runner = new Operation_worksheet();
+		runner.Integer_to_character(98);
+		runner.Print_last_digit(1234);
+		runner.Exact_Average(54,23);
+		
+	}
+}
